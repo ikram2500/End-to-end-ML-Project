b63928a964ccacde2a710b385054a8ac561bbbb4
Muhammad ikram <ikram2500@gmail.com>
2024-01-22 21:50:59 +0500

    model evaluation added

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -2,7 +2,8 @@ from mlProject.constants import *
 from mlProject.entity.config_entity import (DataIngestionConfig,
                                             DataValidationConfig,
                                             DataTransformationConfig,
-                                            ModelTrainerConfig)
+                                            ModelTrainerConfig,
+                                            ModelEvaluationConfig)
 from mlProject.utils.common import read_yaml, create_directories
 
 
@@ -83,3 +84,21 @@ class ConfigurationManager:
             target_column= schema.name
         )
         return model_trainer_config
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evalueation 
+        params = self.params.ElasticNet 
+        schema = self.schema.TARGET_COLUMN
+        
+        create_directories([config.root_dir])
+        
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir = config.root_dir,
+            test_data_path = config.test_data_path,
+            model_path = config.model_path,
+            all_params = params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name
+        )
+        return model_evaluation_config
+    
